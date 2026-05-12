d9315ee7dcb6bd438ff066610700c7bf8e001c95
Ubuntu <ubuntu@ip-172-31-47-167.ap-south-1.compute.internal>
2026-05-12 18:55:16 +0000

    Update selenium chrome

--- test.py
+++ test.py
@@ -1,6 +1,7 @@
 import os
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 
 options = Options()
@@ -9,7 +10,12 @@ options.add_argument("--headless")
 options.add_argument("--no-sandbox")
 options.add_argument("--disable-dev-shm-usage")
 
-driver = webdriver.Chrome(options=options)
+# IMPORTANT: use your actual paths
+options.binary_location = "/usr/bin/google-chrome"
+
+service = Service("/usr/bin/chromedriver")
+
+driver = webdriver.Chrome(service=service, options=options)
 
 file_path = "file://" + os.getcwd() + "/index.html"
 
