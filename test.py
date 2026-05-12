21a65169b4549ef6e1d5d9ffdd42a892acb14246
Ubuntu <ubuntu@ip-172-31-47-167.ap-south-1.compute.internal>
2026-05-12 18:46:25 +0000

    Fix selenium

--- test.py
+++ test.py
@@ -1,8 +1,25 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-driver = webdriver.Chrome()
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+
+options = Options()
+options.binary_location = "/usr/bin/chromium"
+
+options.add_argument("--headless")
+options.add_argument("--no-sandbox")
+options.add_argument("--disable-dev-shm-usage")
+
+service = Service("/usr/bin/chromedriver")
+
+driver = webdriver.Chrome(service=service, options=options)
+
 driver.get("file:///home/ubuntu/devops-project/index.html")
-title = driver.find_element(By.TAG_NAME,"h1").text
+
+title = driver.find_element(By.TAG_NAME, "h1").text
+
 assert "DevOps" in title
+
 print("Test Passed")
+
 driver.quit()
